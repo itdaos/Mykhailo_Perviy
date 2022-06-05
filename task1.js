18cf813685767238dc6786d90a7389ed8b160177
itdaos <jjok730@gmail.com>
2022-06-05 23:48:09 +0300

    task1 completed

--- task1.js
+++ task1.js
@@ -1,5 +1,6 @@
 function filter_list(list) {
-  return []
+  if (!Array.isArray(list)) return;
+  return list.filter((value) => typeof value === 'number') 
 }
 
 const assert = require('assert')
@@ -7,15 +8,19 @@ const assert = require('assert')
 describe("task1", () => {
   describe("#filter_list()", () => {
     it('should properly handle test_case 1', () => {
-      assert.equal(filter_list([1,2,'a','b']), [1,2])
+      assert.deepEqual(filter_list([1,2,'a','b']), [1,2])
     });
 
     it('should properly handle test_case 2', () => {
-      assert.equal(filter_list([1,'a','b',0,15]), [1,0,15])
+      assert.deepEqual(filter_list([1,'a','b',0,15]), [1,0,15])
     });
 
     it('should properly handle test_case 3', () => {
-      assert.equal(filter_list([1,2,'aasf','1','123',123]), [1,2,123])
+      assert.deepEqual(filter_list([1,2,'aasf','1','123',123]), [1,2,123])
     });
+
+    it('should return undefined if `list` is not an Array', () => {
+      assert.equal(filter_list({}), undefined)
+    })
   })
 })
