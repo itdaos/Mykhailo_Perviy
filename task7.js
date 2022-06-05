46038f1653946b1ad40c5e5591144068284786e4
itdaos <jjok730@gmail.com>
2022-06-06 01:16:30 +0300

    task7 completed

--- task7.js
+++ task7.js
@@ -1,5 +1,11 @@
 function bit32ToIPv4(repr32) {
-  return "127.0.0.1"
+  // reeeeally bad code but works 
+  const octet1 = repr32 % 256
+  const octet2 = (repr32 % (256*256)) >> 8
+  const octet3 = (repr32 % (256*256*256)) >> 16
+  let octet4 = (((repr32 % (256*256*256*256)) >> 24) + 256)
+  if (octet4 === 256) octet4 = 0
+  return `${octet4}.${octet3}.${octet2}.${octet1}`
 }
 
 const assert = require('assert')
@@ -17,5 +23,22 @@ describe("task7", () => {
     it('should properly handle test_case 3', () => {
       assert.equal(bit32ToIPv4(0), "0.0.0.0")
     });
+
+    it('should properly handle test_case 4', () => {
+      assert.equal(bit32ToIPv4(2149403361), "128.29.74.225")
+    });
+
+    it('should properly handle test_case 5', () => {
+      assert.equal(bit32ToIPv4(4028451553), "240.29.74.225")
+    });
+
+    it('should properly handle test_case 6', () => {
+      assert.equal(bit32ToIPv4(3357362913), "200.29.74.225")
+    });
+
+    it('should properly handle test_case 7', () => {
+      assert.equal(bit32ToIPv4(1919713), "0.29.74.225")
+    });
+
   })
 })
