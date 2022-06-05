96b88952e61d2b1a12ced227933f3327512291a6
itdaos <jjok730@gmail.com>
2022-06-05 23:49:12 +0300

    task5 completed

--- task5.js
+++ task5.js
@@ -1,5 +1,19 @@
 function transformFriends(friends) {
-  return ""
+  if (typeof friends !== "string") return
+
+  const trasform = friends.split(";")
+  .map((friendStr) => {
+    return friendStr.split(':').map(str => str.toUpperCase())
+  })
+  .map((friendName) => {
+    return [friendName[1], friendName[0]]
+  })
+  .sort((a, b) => ('' + a[0]).localeCompare(b[0]) || ('' + a[1]).localeCompare(b[1]) )
+  .map((friendName) => {
+    return `(${friendName.join(", ")})`
+  })
+  .join("")
+  return trasform
 }
 
 const assert = require('assert')
@@ -8,21 +22,21 @@ describe("task5", () => {
   describe("#transformFriends()", () => {
     it('should properly handle test_case 1', () => {
       assert.equal(
-        transformFriends("Fired:Corwill;Wilfred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"), 
+        transformFriends("Fred:Corwill;Wilfred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"), 
         "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
       )
     });
 
     it('should properly handle test_case 2', () => {
       assert.equal(
-        transformFriends("Fired:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"), 
+        transformFriends("Fred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"), 
         "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
       )
     });
 
     it('should properly handle test_case 3', () => {
       assert.equal(
-        transformFriends("Fired:Corwill;Zaza:Corwill;Wilfred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"), 
+        transformFriends("Fred:Corwill;Zaza:Corwill;Wilfred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"), 
         "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(CORWILL, ZAZA)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
       )
     });
